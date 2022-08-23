b8e3dd42d3cf6dd1ef0f723b254b35aedacc08ff
zacgeis <zacgeis@gmail.com>
2022-08-22 20:30:41 -0500

    Day 1 complete.

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,3 +1,33 @@
+use std::fs;
+use std::num::ParseIntError;
+
 fn main() {
-    println!("Day 1");
+    part1();
+    part2();
+}
+
+fn get_input() -> Vec<u32> {
+    let input = fs::read_to_string("inputs/01.txt").unwrap();
+    let values: Result<Vec<u32>, ParseIntError> = input.lines().map(str::parse).collect();
+    values.unwrap()
+}
+
+fn part1() {
+    let input = get_input();
+
+    let iter1 = input.iter();
+    let iter2 = input.iter().skip(1);
+    let total_count = iter1.zip(iter2).filter(|(a, b)| a < b).count();
+
+    println!("{}", total_count);
+}
+
+fn part2() {
+    let input = get_input();
+
+    let iter1 = input.iter();
+    let iter2 = input.iter().skip(3);
+    let total_count = iter1.zip(iter2).filter(|(a, b)| a < b).count();
+
+    println!("{}", total_count);
 }
