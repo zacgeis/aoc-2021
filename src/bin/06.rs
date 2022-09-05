3cdd1e8b0e3b379ea7035ba92eda4e0c9d5afca2
zacgeis <zacgeis@gmail.com>
2022-09-04 19:31:25 -0500

    Day 6 complete.

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,3 +1,42 @@
+use std::fs;
+
+fn get_input() -> Vec<u8> {
+    let input = fs::read_to_string("inputs/06.txt").unwrap();
+    input
+        .lines()
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|value| value.parse().unwrap())
+        .collect()
+}
+
 fn main() {
-    println!("test");
+    part1();
+    part2();
+}
+
+fn solve(days: u32) -> u64 {
+    let values = get_input();
+    let mut sim: [u64; 9] = Default::default();
+    for value in values {
+        sim[value as usize] += 1;
+    }
+    for _ in 0..days {
+        let expired = sim[0];
+        for i in 1..=8 {
+            sim[i - 1] = sim[i];
+        }
+        sim[6] += expired;
+        sim[8] = expired;
+    }
+    sim.iter().sum::<u64>()
+}
+
+fn part1() {
+    println!("part1: {}", solve(80));
+}
+
+fn part2() {
+    println!("part2: {}", solve(256));
 }
